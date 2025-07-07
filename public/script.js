87fa1bd7c45c279c2c4abfc8e19810439a6c9d33
cceexpress <nomatica@icloud.com>
2025-07-06 21:11:46 -0500

    updatee

--- public/script.js
+++ public/script.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
   mover.style.transition = 'all 0.7s cubic-bezier(.68,-0.55,.27,1.55)';
   mover.style.zIndex = 9999;
   mover.style.boxShadow = '0 0 30px 10px rgba(0,0,0,0.2)';
+  mover.style.opacity = '0.1';
   document.body.appendChild(mover);
   console.log('script.js loaded');
 
